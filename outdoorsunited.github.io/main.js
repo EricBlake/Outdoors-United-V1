9daa272b3df4a8805601e94a4b0be09f5e59663e
Eric Blake <ericblake123@gmail.com>
2016-05-11 15:37:10 -0500

    pushing images and updates to Dashboard

--- outdoorsunited.github.io/main.js
+++ outdoorsunited.github.io/main.js
@@ -1,2 +1,17 @@
+$('.tab').click(function(){
+  $('.tab').removeClass('active');
+  $(this).addClass('active');
+  
+  $('.tab-content').removeClass('active');
+  
+  var toggleId = $(this).attr('data-ui');
+  
+  console.log(toggleId);
+  
+  $('#' + toggleId).addClass('active');
+ 
+   
+});
+
 
 
