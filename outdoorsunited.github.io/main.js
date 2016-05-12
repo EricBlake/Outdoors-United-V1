a2195932ba7eb1ad9039653bb508958280efea35
Eric Blake <ericblake123@gmail.com>
2016-05-12 08:49:58 -0500

    pushing Updates to Profile and Nav

--- outdoorsunited.github.io/main.js
+++ outdoorsunited.github.io/main.js
@@ -15,3 +15,17 @@ $('.tab').click(function(){
 
 
 
+$('.toggle').click(function(){
+  
+//   if($('.menu-content').hasClass('active')){
+//     $('.menu-content').removeClass('active');
+//   }
+  
+//   else {
+//     $('.menu-content').addClass('active');
+//   }  
+  
+  //$('.menu-content').toggleClass('active');
+  
+  $('.menu-content').slideToggle(1000);
+});
